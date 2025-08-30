ea1674463e5b9bbff39945799a0ea4b4baa0d226
forsaken-hero <67844664+forsaken-hero@users.noreply.github.com>
2025-08-30 19:43:37 +0300

    Code style improvement for db.py

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 from flask import g
 
@@ -11,42 +10,28 @@ def get_connection():
         g.db.row_factory = sqlite3.Row
     return g.db
 
-def execute(sql, params=[], con = None):
-    print("db.py's execute starts")
-    if con == None:
+def execute(sql, params=[], con=None):
+    if con is None:
         con = get_connection()
         result = con.execute(sql, params)
         con.commit()
-    else: 
+    else:
         result = con.execute(sql,params)
     g.last_insert_id = result.lastrowid
-    print("db.py's execute succeeded returning last_insert_id",g.last_insert_id, "& con=",con)
     return g.last_insert_id, con
-
 
 def commit(con):
     con.commit()
 
 def query(sql, params=[]):
     con = get_connection()
-    return con.execute(sql, params).fetchall()
+    result = con.execute(sql, params).fetchall()
+    return result
 
 def last_insert_id():
     return g.last_insert_id
 
-def close_connection(e=None):
+def close_connection(exception=None):
     db = g.pop('db', None)
     if db is not None:
         db.close()
-
-'''
-def multi_execute(sql, params=[], con = None): #returning the last insert id & connection
-    print("db.py's multi_execute starts")
-    if con == None: print("con == None!");con = get_connection()
-    print("trying to execute")
-    result = con.execute(sql,params)
-    g.last_insert_id = result.lastrowid
-    print("db.py's multi_execute succeeded, returning con=",con,"& last_insert_id",g.last_insert_id)
-    return g.last_insert_id , con
-
-'''
